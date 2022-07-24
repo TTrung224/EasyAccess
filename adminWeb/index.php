a87ce5cef09ac9ea933c00e3628495e033c9add7
s3877653 <hotranminhkhoi@gmail.com>
2022-07-24 23:01:58 +0700

    add basic frame for the web

--- adminWeb/index.php
+++ adminWeb/index.php
@@ -4,10 +4,34 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <title>Admin Page</title>
 </head>
-<body>
-    <h1>test git</h1>
+<body>  
+    <h1>Access Info</h1>
+    <div class="page-container">
+        <div class="table-div">
+        <table class="main-table">
+            <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Access time</th>
+                <th>Access day</th>
+            </tr>
+            <tr>
+                <td>test</td>
+            </tr>
+        </table>
+        </div>
+        <div class="search-area">
+        <form action="">
+            <p>Search for Student or Staff</p>
+            <label for="search-bar">Enter student or staff id/name</label>
+            <input type="text" id="search-bar">
+            
+        </form>
+        </div>
+    </div>
 </body>
 </html>
 <?php
