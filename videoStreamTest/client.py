8ec0c101e005764ec576e2156efa74fef5887d31
Trung Tran <s3891724@rmit.edu.vn>
2022-07-25 22:14:07 +0700

    modify the client.py

--- videoStreamTest/client.py
+++ videoStreamTest/client.py
@@ -5,14 +5,10 @@ import argparse
 import socket
 import time
 
-# construct the argument parser and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-s", "--server-ip", required=True, help="ip address of the server to which the client will connect")
-args = vars(ap.parse_args())
 
 # initialize the ImageSender object with the socket address of the
 # server
-sender = imagezmq.ImageSender(connect_to="tcp://192.168.2.248:5555".format(args["server_ip"]))
+sender = imagezmq.ImageSender(connect_to="tcp://192.168.2.248:5555")
 
 # get the host name, initialize the video stream, and allow the
 # camera sensor to warmup
