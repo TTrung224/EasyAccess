2f3e9bbe9be59f9788b75a1dc902470117f178f3
s3877653 <hotranminhkhoi@gmail.com>
2022-07-27 22:49:56 +0700

    connect to clearDB test

--- adminWeb/index.php
+++ adminWeb/index.php
@@ -1,3 +1,20 @@
+<?php
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b19dfc7a972b92";
+$password = "5af8fdbd";
+$dbname = "heroku_a0c3ccec1186e2e";
+
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
