3dadf78c15de96aa7c39164f1b619db977b1c55a
s3877653 <hotranminhkhoi@gmail.com>
2022-07-29 16:06:56 +0700

    test UI

--- adminWeb/index.php
+++ adminWeb/index.php
@@ -24,9 +24,51 @@ $num_per_page = 03;
 $start_from = ($page -1)* 03;
 
 //Make access table
-$sql = "SELECT * FROM access limit $start_from, $num_per_page";
+$sql = "SELECT * FROM access a ";
+
+// if(isset($_GET['filter'])){
+//     $id = $_GET['id'];
+//     $startTime = str_replace("T", " ", $_GET['startTime']) . ":00";
+
+//     echo $startTime;
+//     $endTime = $_GET['endTime'];
+//     $query = mysqLi_query($conn,"select * from access where personId ='$id' and dateTime between '$startTime' and '$endTime' order by dateTime ");
+
+// }
+if(isset($_GET['id']) or isset($_GET['startTime']) or isset($_GET['endTime'])){
+    $filters = [];
+        if($_GET['id'] != ""){
+            $id = $_GET['id'];
+            array_push($filters, "a.personId like '$id' ");
+        }
+
+        if($_GET['startTime'] != "" && $_GET['endTime'] != ""){
+            $startTime = str_replace("T", " ", $_GET['startTime']) . ":00";
+            $endTime = str_replace("T", " ", $_GET['endTime']) . ":59";
+            array_push($filters,"a.dateTime >= '$startTime' and a.dateTime <= '$endTime' " );
+        }
+
+
+    if(sizeof($filters)>0){
+        $sql = $sql . "where ";
+        for ($i = 0 ; $i < sizeof($filters); $i++){
+            $sql = $sql . $filters[$i] ;
+            if($i != sizeof($filters) - 1){
+                $sql = $sql . "and ";
+            }
+        }
+    }
+}
+
 $result = mysqLi_query($conn, $sql);
-$nr = mysqLi_num_rows($result);
+$nr = mysqLi_num_rows($result); 
+$total_page = ceil($nr/$num_per_page);
+
+$sql = $sql . "limit $start_from, $num_per_page";
+$result = mysqLi_query($conn, $sql);
+
+echo $sql;
+// id  timerange  abc  paging
 
 
 ?>
@@ -44,11 +86,26 @@ $nr = mysqLi_num_rows($result);
     <div class="page-container">
 
         <div class="search-area">
-            <form action="">
-                <p>Search for Student or Staff</p>
-                <label for="search-bar">Enter student or staff id/name</label>
-                <input type="text" id="search-bar">
+            <form action="index.php" method="get" >
+                <p>Filter access data:</p>
+                <label for="search-bar">Enter student or staff id</label>
+                <input type="text" id="search-bar" name="id"
+                value = "<?= (isset($_GET['id'])) ? $_GET['id'] : ""?>"
+                >
+                <label for="start-time-range">Start date-time</label>
+                <input type="datetime-local" id="start-time-range" name="startTime"
+                value = "<?= (isset($_GET['startTime'])) ? $_GET['startTime'] : ""?>"
+                >
+                <label for="end-time-range">End date-time</label>
+                <input type="datetime-local" id="end-time-range" name="endTime"
+                value = "<?= (isset($_GET['endTime'])) ? $_GET['endTime'] : ""?>"
+                >
+                <input type="submit" name="filter" value="Filter">
+                <button><a href="index.php" class="cancel-btn">Cancel</a></button>
             </form>
+            <?php
+
+            ?>
         </div>
 
         <div class="table-div">
@@ -71,26 +128,34 @@ $nr = mysqLi_num_rows($result);
             </table>
             <div class="page">
             <?php
-            $sql = "SELECT * FROM access";
-            $result = mysqLi_query($conn, $sql);
-            $nr = mysqLi_num_rows($result); 
-            $total_page = ceil($nr/$num_per_page);
-
-            if($page>1){
-                echo "<a href='index.php?page=".($page-1)."' class='Previous-Next-btn'>Previous</a>";
-            }
+            
 
             for($i=1;$i<= $total_page;$i++){
-                echo "<a href='index.php?page=".$i."' class='page-btn'>$i</a>";
+                
+                if(isset($_GET['id']) or isset($_GET['startTime']) or isset($_GET['endTime'])){?>
+                <form action="index.php" method="get">
+                    <input type="hidden" name="id" value=<?=$_GET['id']?>>
+                    <input type="hidden" name="startTime" value=<?=$_GET['startTime']?>>
+                    <input type="hidden" name="endTime" value=<?=$_GET['endTime']?>>
+                    <input type="submit" name="page" value="<?=$i?>" class ="page-btn">
+                </form>
+                <?php } else{
+                    echo "<a href='index.php?page=".$i."' class='page-btn'>$i</a>";
+                }
             }
 
-            if($i >$page){
-                echo "<a href='index.php?page=".($page+1)."' class='Previous-Next-btn'>Next</a>";
-            }
+            
             ?>
             </div>
         </div>
     </div>
+
+    <?php
+    // $a = "2022-07-28T16:55";
+    // $a[10] = " ";
+    // $a = $a . ":00";
+    // echo $a;
+    ?>
 </body>
 </html>
 
