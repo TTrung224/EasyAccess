01af418097546d9a066a3623d65c5fe31303d82f
Trung Tran <s3891724@rmit.edu.vn>
2022-08-01 22:19:15 +0700

    restructure in order to merge

---
+++ loginSystem/config.php
@@ -0,0 +1,12 @@
+<?php
+$servername = "us-cdbr-east-06.cleardb.net";
+$username = "b19dfc7a972b92";
+$password = "5af8fdbd";
+$dbname = "heroku_a0c3ccec1186e2e";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// $conn = mysqli_connect('localhost','root','','user_db');
+
+?>
