5f1818edcb2dd127e48f6fbcf92dd68d692b5421
s3877653 <hotranminhkhoi@gmail.com>
2022-08-09 15:52:55 +0700

    add register pages

--- system/flaskProject/app.py
+++ system/flaskProject/app.py
@@ -1,13 +1,8 @@
 from urllib import response
 from flask import Flask, render_template, Response
-from camera import Video
+from camera import RecogVideo , RegisVideo, RegisWithMaskVideo
 
 app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 def gen(camera):
     while True:
@@ -16,11 +11,35 @@ def gen(camera):
         b'Content-Type: image/jpeg\r\n\r\n' + frame +
          b'\r\n\r\n')
 
-@app.route('/video')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-def video():
-    return Response(gen(Video()),
+@app.route('/recogVideo')
+def recogVideo():
+    return Response(gen(RecogVideo()),
     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/regisVideo')
+def regisVideo():
+    return Response(gen(RegisVideo()),
+    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/regisWithMaskVideo')
+def regisWithMaskVideo():
+    return Response(gen(RegisWithMaskVideo()),
+    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/registration')
+def regisPage():
+    return render_template('registration.html')
+
+@app.route('/regisScan')
+def regisScan():
+    return render_template('regisScan.html')
+
+@app.route('/regisScanWithMask')
+def regisScanWithMask():
+    return render_template('regisScanWithMask.html')
 
 app.run(debug=True)
--- system/flaskProject/camera.py
+++ system/flaskProject/camera.py
@@ -1,6 +1,26 @@
 import cv2
 
-class Video(object):
+class RecogVideo(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    def __del__(self):
+        self.video.release()
+    def get_frame(self):
+        ret,frame = self.video.read()
+        ret,jpg = cv2.imencode('.jpg',frame)
+        return jpg.tobytes()
+
+class RegisVideo(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    def __del__(self):
+        self.video.release()
+    def get_frame(self):
+        ret,frame = self.video.read()
+        ret,jpg = cv2.imencode('.jpg',frame)
+        return jpg.tobytes()
+
+class RegisWithMaskVideo(object):
     def __init__(self):
         self.video = cv2.VideoCapture(0)
     def __del__(self):
