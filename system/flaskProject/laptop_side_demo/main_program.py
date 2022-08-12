aa0685e3a2d3ab66c94882601db8ebecddcf9996
vdVuong0502 <bejamin.vnsharing@gmail.com>
2022-08-12 06:30:55 -0400

    exeption handling

--- system/flaskProject/laptop_side_demo/main_program.py
+++ system/flaskProject/laptop_side_demo/main_program.py
@@ -17,18 +17,22 @@ while True:
 		dict['status'] = True
 		dict['ID'] = 's3878281'
 		dict['name'] = 'Vuong Viet Dung'
-	s = requests.post(server_address, json=json.dumps(dict)).content
-	print(s)
-	while dict['status'] == True:
-		respond =  requests.post(server_check_address)
-		if respond.ok:
-			respond = respond.json()
-			print(respond['status'])
-			if respond['status'] == False:
-				dict = respond
-			time.sleep(1)
-
-	n = n + 5
-	time.sleep(2)
+	try:
+		s = requests.post(server_address, json=json.dumps(dict)).content
+		print(s)
+		while dict['status'] == True:
+			respond =  requests.post(server_check_address)
+			if respond.ok:
+				respond = respond.json()
+				print(respond['status'])
+				if respond['status'] == False:
+					dict = respond
+				time.sleep(1)
+		n = n + 5
+	except requests.exceptions.ConnectionError:
+		time.sleep(1)
+		continue
+	
+	time.sleep(1)
 
 
